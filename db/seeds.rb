2acc98075e421507b8aa410b376c94191a1b4499
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-01-03 20:08:38 -0600

    tested seed data and it was successful

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# user1 = User.create(username: "John Doe", password_digest: "password1", avatar: "jpeg")
+# user2 = User.create(username: "Jane Doe", password_digest: "password2", avatar: "jpeg2")
+
+# group1 = Group.create(name: "Doe Family")
+# group2 = Group.create(name: "Smith Family")
+
+# item1 = Item.create(name: "Milk", image_url: "milk", unit_price: 3.50)
+# item2 = Item.create(name: "Bread", image_url: "bread", unit_price: 2.00)
+# item3 = Item.create(name: "Eggs", image_url: "eggs", unit_price: 2.50)
+
+# inventory1 = Inventory.create(group_id: group1.id, item_id: item1.id , quantity: 5, frequency_of_purchase: "weekly")
+# inventory2 = Inventory.create(group_id: group1.id, item_id: item2.id , quantity: 10, frequency_of_purchase: "weekly")
+# inventory3 = Inventory.create(group_id: group2.id, item_id: item3.id , quantity: 20, frequency_of_purchase: "monthly")
+
+# shopping_list1 = ShoppingList.create(group_id: group1.id, item_id: item1.id, required_quantity: 2)
+# shopping_list2 = ShoppingList.create(group_id: group1.id, item_id: item2.id, required_quantity: 1)
+# shopping_list3 = ShoppingList.create(group_id: group2.id, item_id: item3.id, required_quantity: 5)
+
+# UserGroup.create(user_id: user1.id, group_id: group1.id)
+# UserGroup.create(user_id: user2.id, group_id: group2.id)
