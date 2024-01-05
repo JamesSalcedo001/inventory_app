63a434e64ad92e1ff62500c36f8c2d8de95a23e6
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-01-05 12:20:58 -0600

    added index action to inventories controller and two private methods

--- app/controllers/api/inventories_controller.rb
+++ app/controllers/api/inventories_controller.rb
@@ -1,4 +1,19 @@
 module Api
     class InventoriesController < ApplicationController
+        before_action :set_group
+
+        def index
+            render json: @group.inventories
+        end
+
+        private
+
+        def inventory_params
+            params.permit(:item_id, :quantity, :frequency_of_purchase)
+        end
+
+        def set_group
+            @group = Group.find(params[:id])
+        end
     end    
 end
