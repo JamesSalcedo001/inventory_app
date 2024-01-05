14595c6cffd5d34341eb94ea16c7050b11c43b75
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-01-05 12:57:09 -0600

    added in CRUD actions to groups controller

--- app/controllers/api/groups_controller.rb
+++ app/controllers/api/groups_controller.rb
@@ -1,4 +1,38 @@
 module Api
     class GroupsController < ApplicationController
+        before_action :group_find, only: [:show, :update, :destroy]
+        
+        def index
+            render json: Group.all
+        end
+
+        def show
+            render json: @group
+        end
+
+        def create
+            render json: Group.create!(group_params), status: :created
+        end
+
+        def update
+            @group.update!(group_params)
+            render json: @group
+        end
+
+        def destroy
+            @group.destroy
+            head :no_content
+        end
+
+        private
+
+        def group_params
+            params.permit(:name)
+        end
+
+        def group_find
+            @group = Group.find(params[:id])
+        end
+
     end 
 end
