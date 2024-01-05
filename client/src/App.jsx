3f00fec1e04e03404e525c849390693a42383955
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-01-05 11:25:36 -0600

    added code in signup component and exported

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react'
 import { Route, Routes } from "react-router-dom";
-import Header from './Header';
-import Login from './Login';
+// import Header from './Header';
+// import Login from './Login';
+// import Signup from './Signup';
 import './App.css'
 
 function App() {
---
+++ client/src/Signup.jsx
@@ -0,0 +1,9 @@
+function Signup() {
+    return (
+        <div>
+            <h1>Signup Component here</h1>
+        </div>
+    )
+}
+
+export default Signup;
