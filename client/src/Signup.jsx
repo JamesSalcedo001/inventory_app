a5dbbe68dce536241d92d1e30f0c880d1a4631c1
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-01-05 13:57:54 -0600

    building out standard signup form

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -1,4 +1,49 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { login, setErrors } from "./slices/userSlice";
+import { useNavigate } from "react-router-dom";
+
 function Signup() {
+    const [formData, setFormData] = useState({
+        username: "",
+        password: "",
+        avatar: ""
+    })
+
+    const errors = useSelector(state => state.user.errors)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const { username, password, avatar } = formData
+
+    const submit = (e) => {
+        e.preventDefault()
+        const formDataObj = {
+            username,
+            password,
+            avatar
+        }
+        fetch("/api/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formDataObj)
+        })
+        .then(res => res.json())
+        .then(user => {
+            if (!user.errors) {
+                dispatch(login(user))
+                navigate("/")
+            } else {
+                dispatch(setErrors(user.errors))
+                setTimeout(() => {
+                    dispatch(setErrors([]))
+                }, 2000)
+            }
+        })
+    }
+
+
     return (
         <div>
             <h1>Signup Component here</h1>
