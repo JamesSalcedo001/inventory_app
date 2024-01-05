78f00ebd6be9e1461b6a7788f9d48d47576eb0d2
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-01-05 11:03:33 -0600

    added user actions and routes

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,20 @@
 module Api
     class UsersController < ApplicationController
+
+        def me
+            render json: @current_user, status: :ok
+        end
+
+        def create
+            user = User.create!(user_params)
+            session[:user_id] = user.id
+            render json: user, status: :created
+        end
+
+        private
+
+        def user_params
+            params.permit(:username, :password, :avatar)
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
+    get "/me", to: "users#me"
+    post "/signup", to: "users#create"
   end
 
   get '*path',
