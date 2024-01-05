b7ccffb53c273157b3c234b94fd2e35d2bdfccd4
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-01-05 12:28:46 -0600

    added before action and show action in inventories controller

--- app/controllers/api/inventories_controller.rb
+++ app/controllers/api/inventories_controller.rb
@@ -1,15 +1,25 @@
 module Api
     class InventoriesController < ApplicationController
         before_action :set_group
+        before_action :found_inventory, only: [:show, :update, :destroy]
 
         def index
             render json: @group.inventories
         end
 
+        def show
+            render json: @inventory
+        end
+
+
         private
 
         def inventory_params
             params.permit(:item_id, :quantity, :frequency_of_purchase)
+        end
+
+        def found_inventory
+            @inventory = @group.inventories.find(params[:id])
         end
 
         def set_group
