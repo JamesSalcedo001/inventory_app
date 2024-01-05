5d8632e9c091d0eeb79cbe425ab7e60b19ff2441
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-01-05 11:29:44 -0600

    added signup and login components to app component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react'
 import { Route, Routes } from "react-router-dom";
 import Header from './Header';
 import Login from './Login';
@@ -6,20 +5,14 @@ import Signup from './Signup';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
 
   return (
     <div className="App">
       <Header/>
         <Routes>
-          <Route path="/testing" element={<h1>test route</h1>} />
-          <Route path="/" element={<h1>Page Count: {count}</h1>} />
+          <Route path="/log_in" element={<Login/>} />
+          <Route path="/sign_up" element={<Signup/>} />
+          <Route path="/" element={<h1>Hello</h1>} />
         </Routes>  
     </div>
   )
