137a6c3d31911ac7a8700e403ab814dd777434e2
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-01-05 12:14:23 -0600

    added before action using a private method which queries the database to find an item with a specific id

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,13 +1,30 @@
 module Api
     class ItemsController < ApplicationController
         skip_before_action :authorize, only: [:index, :show]
+        before_action :find_item, only: [:show, :update, :destroy]
 
         def index
             render json: Item.all
         end
 
         def show
-            render json: Item.find(params[:id])
+            render json: @item
+        end
+
+        def create
+            render json: Item.create!(item_params), status: :created
+        end
+
+
+
+        private 
+
+        def item_params
+            params.permit(:name, :image_url, :unit_price)
+        end
+
+        def find_item
+            @item = Item.find(params[:id])
         end
 
     end
